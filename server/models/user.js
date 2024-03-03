ccaa45e8494fb8ea5f83277c8ac138a9af55dbc2
Nguyen Duc Kien <ngdkien12112k3@gmail.com>
2024-03-03 15:17:43 +0700

    #2 Handle error

---
+++ server/models/user.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+
+var userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    mobile: {
+        type: String,
+        unique: true,
+        require: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        default: 'user',
+    },
+    cart: {
+        type: Array,
+        default: [],
+    },
+    // Mảng chứa ID của bảng Address 
+    address: [{type: mongoose.Types.ObjectId, ref: 'Address'}],
+    wishlist: [{ type: mongoose.Types.ObjectId, ref: 'Product' }],
+    isBlocked: {
+        type: Boolean,
+        default: false
+    },
+    refreshToken: {
+        type: String,
+    },
+    passwordChangedAt: {
+        type: String
+    },
+    passwordResetToken: {
+        type: String
+    },
+    passwordResetExpires: {
+        type: String
+    },
+    registerToken: {
+        type: String
+    },
+});
+
+userSchema.pre('save', async function (next) {
+    //Nếu password đã hash rồi thì không hash nữa
+    if (!this.isModified('password')) {
+        next()
+    }
+    const salt = bcrypt.genSaltSync(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+//Export the model
+module.exports = mongoose.model('User', userSchema);
